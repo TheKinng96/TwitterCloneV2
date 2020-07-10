278b40c428e497e761b371d485d88c2b31eb3a31
Gen Yap Feng Yuan <58755531+TheKinng96@users.noreply.github.com>
2020-07-10 21:24:01 +0900

    search is on and all media ready to be shown

--- js/index.js
+++ js/index.js
@@ -1,12 +1,25 @@
 const URL = "http://localhost:3000/tweets";
-
 /**
  * Retrive Twitter Data from API
  */
 const getTwitterData = () => {
+  const query = document.getElementById('user-search-input').value
 
+  if (!query) return;
+  const encodedQuery = encodeURIComponent(query)
+  const fullUrl = `${URL}?q=${encodedQuery}&count=10`
+  fetch(fullUrl).then((response) => {
+    return response.json();
+  }).then((data) => {
+    buildTweets(data.statuses);
+  })
 }
 
+const passByEnter = (e) => {
+  if (e.key == 'Enter') {
+    getTwitterData();
+  }
+}
 /**
  * Save the next page data
  */
@@ -17,6 +30,9 @@ const saveNextPage = (metadata) => {
  * Handle when a user clicks on a trend
  */
 const selectTrend = (e) => {
+  const text = e.innerText;
+  document.getElementById('user-search-input').value = text;
+  getTwitterData();
 }
 
 /**
@@ -29,19 +45,83 @@ const nextPageButtonVisibility = (metadata) => {
  * Build Tweets HTML based on Data from API
  */
 const buildTweets = (tweets, nextPage) => {
+  let twitterContent = "";
+  tweets.forEach((tweet) => {
+    const createdDate = moment(tweet.created_at).fromNow()
+    twitterContent += `
+    <div class="tweet-container">
+      <div class="tweet-user-info">
+          <div class="tweet-user-profile" style="background-image: url(${tweet.user.profile_image_url_https})" >
 
+          </div>
+          <div class="tweet-user-name-container">
+              <div class="tweet-user-fullname">
+                  ${tweet.user.name}
+              </div>
+              <div class="tweet-user-username">
+                  @${tweet.user.screen_name}
+              </div>
+          </div>
+      </div>
+      `
+      if (tweet.entities.media && tweet.entities.media.length > 0) {
+        twitterContent += buildImages(tweet.extended_entities.media);
+        twitterContent += buildVideo(tweet.extended_entities.media)
+      }
+          
+      
+      twitterContent += `
+      <div class="tweet-text-container">
+          ${tweet.full_text}
+      </div>
+      <div class="tweet-date-container">
+          ${createdDate}
+      </div>
+  </div>
+    `
+    document.querySelector('.tweets-list').innerHTML = twitterContent;
+  })
 }
 
 /**
  * Build HTML for Tweets Images
  */
 const buildImages = (mediaList) => {
-
+  let imagesContent = `<div class="tweet-images-container">`;
+  let imageExists = false;
+  mediaList.map((media) => {
+    if (media.type == 'photo'){
+      imageExists = true;
+      imagesContent += `<div class="tweet-image" style="background-image: url(${media.media_url_https})"></div>`
+    }
+  })
+  imagesContent += `</div>`
+  return imageExists ? imagesContent: '';
 }
 
 /**
  * Build HTML for Tweets Video
  */
 const buildVideo = (mediaList) => {
-
+  let videoContent = `<div class="tweet-video-container">`;
+  let videoExists = false;
+  mediaList.map((media) => {
+    if (media.type == 'video'){
+      videoExists = true;
+      videoContent += `
+      <video controls>
+        <source src="${media.video_info.variants[0].url}" type="video/mp4">
+      </video>
+      `
+    } else if (media.type == 'animated_gif') {
+      videoExists = true;
+      videoContent += `
+      <video loop autoplay>
+        <source src="${media.video_info.variants[0].url}" type="video/mp4">
+      </video>
+      `
+    }
+  })
+  videoContent += `</div>`
+  return videoExists ? videoContent: '';
 }
